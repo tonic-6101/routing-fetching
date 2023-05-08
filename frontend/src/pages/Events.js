51b5bf0745222b557d46075e3f072e1ac1279b9d
tonic-6101 <tonicxx341@gmail.com>
2023-05-08 10:14:57 +0200

    added loader()

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,7 +3,10 @@ import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
+  // const data = useLoaderData();
   const events = useLoaderData();
+
+  // const events = data;
 
   return <EventsList events={events} />;
 }
@@ -12,11 +15,11 @@ export default EventsPage;
 
 export async function loader() {
   const response = await fetch('http://localhost:8080/events');
+  console.log(response);
 
   if (!response.ok) {
-    // ...
+    return { isError: true, message: 'Could not fetch events.' };
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
