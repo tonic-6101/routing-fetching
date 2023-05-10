eb920802274aaabeff91e08cb4eed0d759edcdce
tonic-6101 <tonicxx341@gmail.com>
2023-05-10 09:15:15 +0200

    added submitting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import EditEventPage from './pages/EditEvent';
 import EventDetailPage, {
   loader as eventDetailLoader,
+  action as deleteEventAction,
 } from './pages/EventDetail';
 import EventsPage, { loader as eventsLoader, loader } from './pages/Events';
 import EventsRootLayout from './pages/EventsRoot';
@@ -35,6 +36,7 @@ const router = createBrowserRouter([
               {
                 index: true,
                 element: <EventDetailPage />,
+                action: deleteEventAction,
               },
               { path: 'edit', element: <EditEventPage /> },
             ],
